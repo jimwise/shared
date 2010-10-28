bf53f91dd7218901c43249366715e3b70107b9cc
Jim Wise <jwise@draga.com>
2010-10-28 11:38:59 -0400

    Make clear that this is work in progress.

--- nondeterminism/c/example.c
+++ nondeterminism/c/example.c
@@ -1,37 +1,68 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "nondeterminism.h"
 
-
-int
+/* Choose 2 example  (see _On Lisp_, sec, 22.1 (pp. 286-289) */
+void
 choose_2 (void) {
   int x;
-  int ch[2] = {2, 1};
+  int ch[] = {2, 1};
   CHOOSE(x, 2, ch);
-  if (x == 1) {
+  if (x != 2) {
     FAIL();
   }
   printf("we chose: %d\n", x);
 }
 
-int
+/* Parlor Trick example (see _On Lisp_, sec, 22.2 (pp. 290-292) */
+void
 parlor_trick (int sum) {
   int x1, x2;
-  int ch[6] = {0, 1, 2, 3, 4, 5};
+  int ch[] = {0, 1, 2, 3, 4, 5};
   CHOOSE(x1, 6, ch);
   CHOOSE(x2, 6, ch);
-  if ((x1 + x2) == sum)
+  if ((x1 + x2) == sum) {
     printf("%d is the sum of %d and %d\n", sum, x1, x2);
-  else
+  } else {
     FAIL();
+  }
+}
+
+/* Chocoblob Coin Search example (with cuts) (see _On Lisp_, sec, 22.5 (pp. 298-302) */
+int
+coinp (char *city, int store, int box) {
+  return((!strcmp(city, "la") && store == 1 && box == 2) ||
+	 (!strcmp(city, "ny") && store == 1 && box == 1) ||
+	 (!strcmp(city, "bos") && store == 2 && box == 2));
+}
+
+void
+find_boxes (void) {
+  char *city;
+  char *cities[] = {"bos", "ny", "la"};
+  int box, store;
+  int oneortwo[] = {2, 1};
+
+  CHOOSE(city, 3, cities);
+  MARK();
+  CHOOSE(store, 2, oneortwo);
+  CHOOSE(box, 2, oneortwo);
+  printf("(%s %d %d) ", city, store, box);
+  if (coinp(city, store, box)) {
+    CUT();
+    printf("C ");
+  }
+  FAIL();
 }
 
 int
 main (int argc, char **argv) {
-  ND_INIT;
   choose_2();
-
+  ND_RESET();
   parlor_trick(7);
+  ND_RESET();
+  find_boxes();
   exit(0);
 }
--- nondeterminism/c/nondeterminism.h
+++ nondeterminism/c/nondeterminism.h
@@ -7,27 +7,51 @@ extern "C" {
 
 #include <setjmp.h>
 
-#define MAX_DEPTH 100
+#define ND_MAX_DEPTH 100
 
+/*
+ * WARNING:  Implementation specific:                                        
+ *  we assume an uninitialized jmpbuf is != any initialized jmpbuf!
+ */
 int _currpath = -1;
-jmp_buf _paths[MAX_DEPTH];
+jmp_buf _paths[ND_MAX_DEPTH];
+jmp_buf _cut_marker;
 
-  /* chcs is int[n], x gets choice */
+#define ND_RESET() _currpath = -1; /* setjmp(_paths[0]); _currpath = 0; */
+
+  /* chcs is T[n], x gets choice */
 #define CHOOSE(x, n, chcs)			\
   {						\
+    /* printf("CHOOSE\n"); */			\
     int _chc = n - 1;				\
     setjmp(_paths[++_currpath]);		\
+    /* printf("_chc = %d\n", _chc); */		\
     if (_chc == -1) {				\
-      _currpath--;				\
+      --_currpath;				\
       FAIL();					\
-    } else {					\
-      x = chcs[_chc--];				\
     }						\
+    x = chcs[_chc--];				\
   }
   
-#define FAIL() longjmp(_paths[_currpath], -1)
+#define FAIL()						\
+  {							\
+    /* printf("FAIL: _currpath = %d\n", _currpath); */	\
+    if (_paths[_currpath] == _cut_marker) {		\
+      _currpath--;					\
+    }							\
+    if (_currpath > -1) {				\
+      longjmp(_paths[_currpath], -1);			\
+    }							\
+  }
 
-#define ND_INIT  setjmp(_paths[++_currpath])
+#define MARK() memcpy(_paths[++_currpath], _cut_marker, sizeof(jmp_buf));
+
+#define CUT()								\
+  {									\
+    while ((_currpath > -1) && (_paths[_currpath--] != _cut_marker))	\
+      ; /* do nothing */						\
+    /* _paths has now been unwound back to before last _cut_marker */	\
+  }
 
 #ifdef  __cplusplus
 }
