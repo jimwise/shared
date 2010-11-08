e60ac321af3b90b49db7142ef1a67a5b031f419c
Jim Wise <jwise@draga.com>
2010-11-08 16:18:31 -0500

    Merge generation.c into life.c

--- vlife/conway/life.c
+++ vlife/conway/life.c
@@ -160,3 +160,44 @@ run (void) {
     sleep(1);
   }
 }
+
+
+/*
+ * generation() -- given two board selectors, generate the next generation
+ * from the first board into the second board.
+ */
+
+void
+generation (int from, int to) {
+  int index, xedni;
+	
+  for (index=1; index<=YMAX; index++)
+    for (xedni=1; xedni<=XMAX; xedni++)
+      world[to][index][xedni] = determine(from, index, xedni);
+}
+
+/*
+ * determine() -- given a board selector, and the coordinates of a square,
+ * return the state of the square in the next generation.
+ */
+
+int
+determine (int which, int row, int col) {
+  int	count=0;
+  /* XXX XXX XXX pseudo-dynamic scope --depends on which naming the paramter here*/
+  count = CELL(row-1, col-1) + CELL(row-1, col) + CELL(row-1, col+1) +
+    CELL(row, col-1) + CELL(row, col+1) +
+    CELL(row+1, col-1) + CELL(row+1, col) + CELL(row+1, col+1);
+	
+  if (CELL(row, col))	{
+    if ((count == 2) || (count == 3))
+      return 1;
+    else
+      return 0;
+  } else {
+    if (count == 3)
+      return 1;
+    else
+      return 0;
+  }
+}
