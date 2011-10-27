cf46cb0c26b182fe1bf6670c864c3d1b88178cd6
Jim Wise <jwise@draga.com>
2011-10-27 14:48:52 -0400

    Close over deck and (new) random state.
    Cleaner, and fixes shuffle on lisps where *random-state* isn't automatically
    random.

--- blackjack/lisp/cards.lisp
+++ blackjack/lisp/cards.lisp
@@ -18,15 +18,15 @@
 	(s '() (append s +one-deck+)))
        ((= i *decksinshoe*) s))))
 
-(defparameter *shoe* '())
 
-(defun shuffle ()
-  (format t "Refilling shoe with ~a decks~%" *decksinshoe*)
-  (setf *shoe* (make-shoe)))
-
-(defun draw ()
-  (when (null *shoe*) (shuffle))
-  (pop *shoe*))
+(let ((shoe ()))
+  (defun shuffle ()
+    (format t "Refilling shoe with ~a decks~%" *decksinshoe*)
+    (setf shoe (make-shoe)))
+     
+  (defun draw ()
+    (when (null shoe) (shuffle))
+    (pop shoe)))
 
 ;; Knuth Shuffle
 ;; from Rosetta Code:
@@ -36,15 +36,16 @@
   (etypecase sequence
     (list  (nshuffle-list sequence))
     (array (nshuffle-array sequence))))
+
+(let ((rs (make-random-state t))) 
+  (defun nshuffle-list (list)
+    "Shuffle the list using an intermediate vector."
+    (let ((array (nshuffle-array (coerce list 'vector))))
+      (declare (dynamic-extent array))
+      (map-into list 'identity array)))
  
-(defun nshuffle-list (list)
-  "Shuffle the list using an intermediate vector."
-  (let ((array (nshuffle-array (coerce list 'vector))))
-    (declare (dynamic-extent array))
-    (map-into list 'identity array)))
- 
-(defun nshuffle-array (array)
-  (loop for i from (length array) downto 2
-        do (rotatef (aref array (random i))
-                    (aref array (1- i)))
-        finally (return array)))
+  (defun nshuffle-array (array)
+    (loop for i from (length array) downto 2
+       do (rotatef (aref array (random i rs))
+		   (aref array (1- i)))
+       finally (return array))))
