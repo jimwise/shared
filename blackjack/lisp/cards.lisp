f8f0cbcc28f79b34fa0ac14c53673573099fa843
Jim Wise <jwise@draga.com>
2011-10-27 15:28:14 -0400

    Now that we're in a package, don't need to get clever with scope.

--- blackjack/lisp/cards.lisp
+++ blackjack/lisp/cards.lisp
@@ -4,16 +4,11 @@
 
 (in-package :cards)
 
-(defparameter *decksinshoe* 6)
-;; should be defconstant, but for REPL use
-(defparameter +suits+ '(hearts diamonds clubs spades))
-(defparameter +cards+ '(ace two three four five six seven eight nine ten jack queen king))
+(defconstant *decksinshoe* 6)
+(defconstant +suits+ '(hearts diamonds clubs spades))
+(defconstant +cards+ '(ace two three four five six seven eight nine ten jack queen king))
 
 (defstruct card val suit)
-
-(defparameter +one-deck+
-  (loop for s in +suits+ appending
-       (loop for v in +cards+ collecting (make-card :val v :suit s))))
 
 (defun card-ord (c)
   (1+ (position (card-val c) +cards+)))
@@ -21,21 +16,24 @@
 (defun card-name (c)
   (format nil "~a of ~a" (card-val c) (card-suit c)))
 
+(defconstant +one-deck+
+  (loop for s in +suits+ appending
+       (loop for v in +cards+ collecting (make-card :val v :suit s))))
+
 (defun make-shoe ()
   (nshuffle
    (do ((i 1 (+ i 1))
 	(s '() (append s +one-deck+)))
        ((= i *decksinshoe*) s))))
 
-
-(let ((shoe ()))
-  (defun shuffle ()
-    (format t "Refilling shoe with ~a decks~%" *decksinshoe*)
-    (setf shoe (make-shoe)))
+(defvar shoe ())
+(defun shuffle ()
+  (format t "Refilling shoe with ~a decks~%" *decksinshoe*)
+  (setf shoe (make-shoe)))
      
-  (defun draw ()
-    (when (null shoe) (shuffle))
-    (pop shoe)))
+(defun draw ()
+  (when (null shoe) (shuffle))
+  (pop shoe))
 
 ;; Knuth Shuffle
 ;; from Rosetta Code:
@@ -46,15 +44,16 @@
     (list  (nshuffle-list sequence))
     (array (nshuffle-array sequence))))
 
-(let ((rs (make-random-state t))) 
-  (defun nshuffle-list (list)
-    "Shuffle the list using an intermediate vector."
-    (let ((array (nshuffle-array (coerce list 'vector))))
-      (declare (dynamic-extent array))
-      (map-into list 'identity array)))
+(defvar rs (make-random-state t))
+
+(defun nshuffle-list (list)
+  "Shuffle the list using an intermediate vector."
+  (let ((array (nshuffle-array (coerce list 'vector))))
+    (declare (dynamic-extent array))
+    (map-into list 'identity array)))
  
-  (defun nshuffle-array (array)
-    (loop for i from (length array) downto 2
-       do (rotatef (aref array (random i rs))
-		   (aref array (1- i)))
-       finally (return array))))
+(defun nshuffle-array (array)
+  (loop for i from (length array) downto 2
+     do (rotatef (aref array (random i rs))
+		 (aref array (1- i)))
+     finally (return array)))
