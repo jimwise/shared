c06b287331a7a48970f27e6ee3d7e089e1ceb06c
Jim Wise <jwise@draga.com>
2014-02-05 17:21:32 -0500

    Make idiomatic, update style.
    I've written a lot of Ruby since I wrote this.  Apply this fact.

--- rpn/ruby/rpn.rb
+++ rpn/ruby/rpn.rb
@@ -16,10 +16,10 @@ module RPNCalc
 
     def call stack
       if stack.size < @arity
-        RPNCalc::signal_error "stack underflow"
+        RPNCalc.signal_error "stack underflow"
         stack
       else
-        @bod.call(stack)
+        @bod.call stack
       end
     end
   end
@@ -30,64 +30,72 @@ module RPNCalc
       @stack = []
       @ops = {}
 
-      self.add_op(".", 1, "display top value on the stack") {|stack|
+      add_op ".", 1, "display top value on the stack" do |stack|
         puts stack[0]
-      }
-      self.add_op("#", 0, "display number of values on the stack") {|stack|
+      end
+
+      add_op "#", 0, "display number of values on the stack" do |stack|
         puts stack.size
-      }
-      self.add_op("+", 1, "replace top two values on the stack with their sum") {|stack|
+      end
+
+      add_op "+", 1, "replace top two values on the stack with their sum" do |stack|
         stack[0,2] = stack[1] + stack[0]
-      }
-      self.add_op("-", 1, "replace top two values on the stack with their difference") {|stack|
+      end
+
+      add_op "-", 1, "replace top two values on the stack with their difference" do |stack|
         stack[0,2] = stack[1] - stack[0]
-      }
-      self.add_op("*", 1, "replace top two values on the stack with their product") {|stack|
+      end
+
+      add_op "*", 1, "replace top two values on the stack with their product" do |stack|
         stack[0,2] = stack[1] * stack[0]
-      }
-      self.add_op("/", 1, "replace top two values on the stack with their quotient") {|stack|
+      end
+
+      add_op "/", 1, "replace top two values on the stack with their quotient" do |stack|
         stack[0,2] = stack[1] / stack[0]
-      }
-      self.add_op("^", 1, "replace top two values on the stack, x and y, with x to the yth power") {|stack|
+      end
+
+      add_op "^", 1, "replace top two values on the stack, x and y, with x to the yth power" do |stack|
         stack[0,2] = stack[1] ** stack[0]
-      }
-      self.add_op("drop", 1, "remove top value from the stack") {|stack|
-        stack[0,1] = []
-      }
-      self.add_op("dup", 1, "duplicate top value on the stack") {|stack|
+      end
+
+      add_op "drop", 1, "remove top value from the stack" do |stack|
+        stack.slice! 0
+      end
+
+      add_op "dup", 1, "duplicate top value on the stack" do |stack|
         stack.unshift stack[0]
-      }
-      self.add_op("swap", 1, "swap top two values on the stack") {|stack|
+      end
+
+      add_op "swap", 1, "swap top two values on the stack" do |stack|
         stack[0], stack[1] = stack[1], stack[0]
-      }
-      self.add_op("help", 0, "display this help") {|stack|
+      end
+
+      add_op "help", 0, "display this help" do |stack|
         puts "#{@ops.size} Commands:"
-        @ops.map {|k, v|
+        @ops.map do |k, v|
           puts "  #{k} -- #{v.doc}"
-        }
-      }
-    end
-
-    def add_op name, arity, doc, &bod
-      ops[name] = Op.new(arity, doc, &bod)
-    end
-
-    def action str
-      if @ops[str]
-        @ops[str].call(@stack)
-      else
-        begin
-          @stack.unshift(Float(str))
-        rescue
-          RPNCalc::signal_error("unknown operation")
         end
       end
     end
 
+    def add_op name, arity, doc, &bod
+      ops[name] = Op.new arity, doc, &bod
+    end
+
+    def action str
+      if @ops[str]
+        @ops[str].call @stack
+      else
+          @stack.unshift Float str
+      end
+    rescue
+      RPNCalc.signal_error "unknown operation"
+    end
+
     def repl
       print "> "
-      while s = gets do
-        self.action s.strip
+      STDIN.each do |s|
+        action s.strip
         print "> "
       end
       puts
